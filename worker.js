a04849e2065b1674ac99bb5ccd41e6b2a203ccc2
Xue Song <Cedar_1012@163.com>
2026-09-15 14:52:26 +0800

    Point login and save API to api.cedar.beer.
    This avoids workers.dev for users who cannot reach that hostname.

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const CORS = {
-  "Access-Control-Allow-Origin": "https://cedar1012.github.io",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, x-user, x-pass",
 };
